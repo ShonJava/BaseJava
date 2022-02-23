cb4ff5cdbae833b2b1bd1e7d6478e30f535fb48e
ShonJava <f5df01d29806@gmail.com>
2022-02-23 20:00:40 +0500

    BJ hw 1 не компилируется, куча ошибок второй вариант

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,29 +8,36 @@ import com.urise.webapp.model.Resume;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int size = storage.length;
+    int size = 0;
 
     public void clear() {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    public void update(Resume resume) {
-        System.out.println("Error");
+    public void update(Resume r) {
+        for(int i = 0; i < size; i++) {
+            if (storage[i].getUuid() == r.getUuid()) {
+                return;
+            } else {
+                System.out.println("Error");
+            }
+        }
+        return;
     }
 
     public void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = r;
         }
 
     }
 
     public Resume get(String uuid) {
-        for(int i = 0; i < storage.length; i++) {
-            if (storage[i].uuid == uuid) {
+        for(int i = 0; i < size; i++) {
+            if (storage[i].getUuid() == uuid) {
                 return storage[i];
             }
         }
@@ -52,7 +59,7 @@ public class ArrayStorage {
      */
     public Resume[] getAll() {
         Resume[] result = new Resume[size];
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             result[i] = storage[i];
         }
         return result;
