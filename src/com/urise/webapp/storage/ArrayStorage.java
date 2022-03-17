18fae4745deece7b4e905c74265bb7a5030a5c51
ShonJava <f5df01d29806@gmail.com>
2022-03-17 22:47:27 +0500

    hw 1 use arraylist methods.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,70 +2,69 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    ArrayList<Resume> storage = new ArrayList<Resume>(STORAGE_LIMIT);
     int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(new ArrayList[]{storage}, 0, size, null);
         size = 0;
     }
 
     public void update(Resume r) {
-        for(int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == r.getUuid()) {
-                return;
-            } else {
-                System.out.println("Error");
-            }
-        }
-        return;
+
     }
 
     public void save(Resume r) {
-        for (int i = 0; i < size; i++) {
-            storage[i] = r;
+        if (storage.indexOf(r) != -1) {
+            System.out.println("This uuid" + r.getUuid() + " is already");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Overflow");
+        } else {
+            storage.add(size, r);
+            System.out.println("you save uuid");
         }
-        size++;
     }
 
     public Resume get(String uuid) {
-        for(int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == uuid) {
-                return storage[i];
+        for (int i = 0; i <= size; i++) {
+            Resume r = storage.get(i);
+            if (r.getUuid() == uuid) {
+                return storage.get(i);
+            } else {
+                System.out.println("not find resume");
             }
         }
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if(uuid == storage[i].getUuid()) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
+        for (int i = 0; i <= size; i ++) {
+            Resume r = storage.get(i);
+            if (r.getUuid().equals(uuid)) {
+                storage.remove(i);
             }
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         Resume[] result = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = storage[i];
-        }
+        result = (Resume[]) storage.clone();
         return result;
     }
 
     public int size() {
         return size;
     }
+
+    private int getIndex(Resume r) {
+        return storage.indexOf(r);
+    }
+
 }
+
+
---
+++ src/com/urise/webapp/storage/Storage.java
@@ -0,0 +1,21 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public interface Storage {
+
+        public void clear();
+
+        public void update(Resume r);
+
+        public void save(Resume r);
+
+        public Resume get(String uuid);
+
+        public void delete(String uuid);
+
+        public Resume[] getAll();
+
+        public int size();
+
+}
