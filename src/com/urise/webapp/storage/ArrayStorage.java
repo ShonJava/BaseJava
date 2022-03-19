5536c113adc9d913911de7eef05af02f5610ba48
ShonJava <f5df01d29806@gmail.com>
2022-03-19 12:28:09 +0500

    Realized all methods, working

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,58 +2,62 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayStorage {
-    private static final int STORAGE_LIMIT = 10000;
-    ArrayList<Resume> storage = new ArrayList<Resume>(STORAGE_LIMIT);
+
+    Resume[] storage = new Resume[10000];
     int size = 0;
 
     public void clear() {
-        Arrays.fill(new ArrayList[]{storage}, 0, size, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void update(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("not find resume " + r.getUuid());
+        } else {
+            storage[index] = r;
+        }
     }
 
     public void save(Resume r) {
-        if (storage.indexOf(r) != -1) {
-            System.out.println("This uuid" + r.getUuid() + " is already");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Overflow");
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            storage[size] = r;
+            size++;
+        } else if (index >= size) {
+            System.out.println("Resume[] overflow");
         } else {
-            storage.add(size, r);
-            System.out.println("you save uuid");
+            System.out.println(r.getUuid() + " already");
         }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i <= size; i++) {
-            Resume r = storage.get(i);
-            if (r.getUuid() == uuid) {
-                return storage.get(i);
-            } else {
-                System.out.println("not find resume");
-            }
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Error, resume not find");
+            return null;
         }
-        return null;
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i <= size; i ++) {
-            Resume r = storage.get(i);
-            if (r.getUuid().equals(uuid)) {
-                storage.remove(i);
+        int index = getIndex(uuid);
+            if (index == -1) {
+                System.out.println("Error, not find resume");
+            } else {
+                storage[index] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
-        }
     }
 
     public Resume[] getAll() {
         Resume[] result = new Resume[size];
-        result = (Resume[]) storage.clone();
+        result = Arrays.copyOfRange (storage, 0, size);
         return result;
     }
 
@@ -61,8 +65,13 @@ public class ArrayStorage {
         return size;
     }
 
-    private int getIndex(Resume r) {
-        return storage.indexOf(r);
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
